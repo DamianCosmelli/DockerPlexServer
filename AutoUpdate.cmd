c9e53b07fdef75268ef21d8152fe2d329902414e
Damián Cosmelli <damiancosmelli@gmail.com>
2023-10-01 04:13:52 -0300

    Update Paths

--- AutoUpdate.cmd
+++ AutoUpdate.cmd
@@ -1,11 +1,11 @@
 @echo off
 REM Baja contenedor de PlexServer
-docker-compose -f E:\DockerPlexServer\docker-compose.yaml down -v
+docker-compose -f C:\Dockers\DockerPlexServer\docker-compose.yaml down -v
 
 REM Descarga ultima version de  PlexServer
 docker pull ghcr.io/linuxserver/plex
 
 REM Inicia server de PlexServer
-docker-compose -f E:\DockerPlexServer\docker-compose.yaml up -d
+docker-compose -f C:\Dockers\DockerPlexServer\docker-compose.yaml up -d
 
 exit
